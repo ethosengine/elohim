9e3edd8dfa5f422bc8d6409e286b27e23f8ea767
Matthew Dowell <mbd06b+github@gmail.com>
2025-08-15 03:45:15 +0000

    add jenkinsfile and fix test

--- elohim-app/src/app/app.component.spec.ts
+++ elohim-app/src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, elohim-app');
+    expect(compiled.textContent).toContain('Elohim Protocol');
   });
 });
